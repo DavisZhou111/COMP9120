276e33be33e8ae88edd010bb5c5830ba73bb3d32
DavisZhou111 <140677595+DavisZhou111@users.noreply.github.com>
2024-09-26 16:03:51 +1000

    9.26

--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import psycopg2
 
 #####################################################
@@ -10,8 +9,8 @@ Connect to the database using the connection string
 '''
 def openConnection():
     # connection parameters - ENTER YOUR LOGIN AND PASSWORD HERE
-    userid = "y24s2c9120_unikey"
-    passwd = ""
+    userid = "y24s2c9120_yzho5104"
+    passwd = "zys141414.."
     myHost = "awsprddbs4836.shared.sydney.edu.au"
 
 
@@ -30,13 +29,35 @@ def openConnection():
     # return the connection to use
     return conn
 
+# def testConnection(conn):
+#     if conn is not None:
+#         try:
+#             # Create a cursor to perform database operations
+#             cur = conn.cursor()
+#             # Executing a query
+#             cur.execute("SELECT version();")
+#             # Fetch result
+#             db_version = cur.fetchone()
+#             print("Connected to PostgreSQL server:", db_version)
+#             cur.close()
+#         except psycopg2.Error as e:
+#             print("Error executing query:", e)
+#     else:
+#         print("Connection was not established")
+#
+# if __name__ == "__main__":
+#     conn = openConnection()
+#     testConnection(conn)
+#     if conn is not None:
+#         conn.close()
+#         print("Database connection closed.")
+
 '''
 Validate staff based on username and password
 '''
 def checkLogin(login, password):
 
     return ['jdoe', 'John', 'Doe', 'jdoe@csh.com']
-
 
 '''
 List all the associated admissions records in the database by staff
